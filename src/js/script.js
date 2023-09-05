33e6eb7e25167076f3ae19adf1a43efeb393b28c
w-udhav <udhavwadhawan@hotmail.com>
2023-09-05 15:20:48 +0530

    update: ui ready 80%

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Dashboard
 const catDisplay = document.querySelector("#catDisplay");
 function catDisplay_interval() {
   let arr = ["Electronics", "Clothing", "Home", "Furniture", "Books", "Health"];
@@ -11,3 +12,31 @@ function catDisplay_interval() {
   }, 3000);
 }
 catDisplay_interval();
+
+// Sidebar
+const buttons = document.querySelectorAll("#links button");
+const components = document.querySelectorAll(".content-component");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const targetComponentId = button.getAttribute("data-target");
+
+    // Hide all components and show only the target component
+    components.forEach((component) => {
+      if (component.id === targetComponentId) {
+        component.style.display = "flex";
+      } else {
+        component.style.display = "none";
+      }
+    });
+
+    // Remove active class from all buttons and add it to the clicked button
+    buttons.forEach((btn) => {
+      if (btn === button) {
+        btn.classList.add("active");
+      } else {
+        btn.classList.remove("active");
+      }
+    });
+  });
+});
